95315c9970acea358abab0835e5797bdaa30b4a4
Felipe Ferraz <felipe.ferraz.90@hotmail.com>
2026-05-27 21:16:28 -0300

    feat(user): add email and username uniqueness validation to user creation

--- models/user.js
+++ models/user.js
@@ -1,6 +1,14 @@
 import database from "infra/database.js";
+import { ValidationError } from "infra/errors.js";
 
 async function create(userInputValues) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+}
+
+async function runInsertQuery(userInputValues) {
   const result = await database.query({
     text: `INSERT INTO users (username, email, password)
            VALUES ($1, $2, $3)
@@ -12,6 +20,34 @@ async function create(userInputValues) {
     ],
   });
   return result.rows[0];
+}
+
+async function validateUniqueEmail(email) {
+  const result = await database.query({
+    text: `SELECT COUNT(*) FROM users WHERE LOWER(email) = LOWER($1);`,
+    values: [email],
+  });
+  if (result.rows[0].count > 0) {
+    throw new ValidationError({
+      message: "Email already in use",
+      action: "Use a different email",
+    });
+  }
+  return true;
+}
+
+async function validateUniqueUsername(username) {
+  const result = await database.query({
+    text: `SELECT COUNT(*) FROM users WHERE LOWER(username) = LOWER($1);`,
+    values: [username],
+  });
+  if (result.rows[0].count > 0) {
+    throw new ValidationError({
+      message: "Username already in use",
+      action: "Use a different username",
+    });
+  }
+  return true;
 }
 
 const user = {
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -2,8 +2,6 @@ import orchestrator from "tests/orchestrator.js";
 import { version as uuidVersion } from "uuid";
 const route = "/api/v1/users";
 const url = `${process.env.BASE_URL}${route}`;
-let response;
-let respBody;
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -11,24 +9,22 @@ beforeAll(async () => {
   await orchestrator.runPendingMigrations();
 });
 
-beforeEach(async () => {
-  response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: "john_doe",
-      email: "john_doe@example.com",
-      password: "123456",
-    }),
-  });
-  respBody = await response.json();
-});
 describe("API v1", () => {
   describe("POST /users", () => {
     describe("Anonymous user", () => {
       test("With unique and valid data", async () => {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "john_doe",
+            email: "john_doe@example.com",
+            password: "123456",
+          }),
+        });
+        const respBody = await response.json();
         expect(response.status).toBe(201);
         expect(respBody).toEqual({
           id: respBody.id,
@@ -41,6 +37,76 @@ describe("API v1", () => {
         expect(Date.parse(respBody.created_at)).not.toBeNaN();
         expect(Date.parse(respBody.updated_at)).not.toBeNaN();
         expect(respBody.created_at).toEqual(respBody.updated_at);
+      });
+
+      test("Would not create with duplicated email", async () => {
+        const response1 = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "duplicated_email_user1",
+            email: "duplicated@example.com",
+            password: "1234567890",
+          }),
+        });
+        expect(response1.status).toBe(201);
+
+        const response2 = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "duplicated_email_user2",
+            email: "Duplicated@example.com",
+            password: "1234567890",
+          }),
+        });
+        expect(response2.status).toBe(400);
+        const respBody2 = await response2.json();
+        expect(respBody2).toEqual({
+          statusCode: 400,
+          name: "ValidationError",
+          message: "Email already in use",
+          action: "Use a different email",
+        });
+      });
+
+      test("Would not create with duplicated username", async () => {
+        const response1 = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "duplicated_user",
+            email: "duplicated1@example.com",
+            password: "1234567890",
+          }),
+        });
+        expect(response1.status).toBe(201);
+
+        const response2 = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: "dupLicated_user",
+            email: "duplicated3@example.com",
+            password: "123456ss7890",
+          }),
+        });
+        expect(response2.status).toBe(400);
+        const respBody2 = await response2.json();
+        expect(respBody2).toEqual({
+          statusCode: 400,
+          name: "ValidationError",
+          message: "Username already in use",
+          action: "Use a different username",
+        });
       });
     });
   });
